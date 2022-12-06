b7fd618485c9960e0177e945fff13afc5df5fae3
SAMBIT SARGAM EKALABYA <sambitsargam2003@gmail.com>
2022-12-06 07:54:23 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,15 +1,37 @@
-mylist = []
-size = int(input('How many elements you want to enter? '))
 
-print('Enter',str(size),'positive numbers')
+import string
 
-for i in range(size):
-    data = int(input())
-    mylist.append(data)
+counts = 0                          # Initialize variables
+dictionary_counts = dict()
+relative_lst = list()
 
-max = 0
-for data in mylist:
-    if data > max:
-        max = data
+fname = input('Enter file name: ')
+try:
+    fhand = open(fname)
+except FileNotFoundError:
+    print('File cannot be opened:', fname)
+    exit()
 
-print('The largest number in list is', max)
+for line in fhand:
+    line = line.translate(str.maketrans('', '', string.digits))
+    line = line.translate(str.maketrans('', '', string.punctuation))
+    line = line.lower()
+
+    # Removes numbers and punctuation then sets all letters to lower case
+    words = line.split()
+    for word in words:
+        for letter in word:
+            # Count each letter for relative frequencies
+            counts += 1
+            if letter not in dictionary_counts:
+                dictionary_counts[letter] = 1
+            else:
+                dictionary_counts[letter] += 1
+
+for key, val in list(dictionary_counts.items()):
+    relative_lst.append((val / counts, key))  # Computes the relative frequency
+
+relative_lst.sort(reverse=True)         # Sorts from highest rel freq
+
+for key, val in relative_lst:
+    print(key, val)
