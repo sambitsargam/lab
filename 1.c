95e20a112e2460d93289e42b026c068f1c4bfcde
SAMBIT SARGAM EKALABYA <sambitsargam2003@gmail.com>
2022-12-20 21:46:16 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,59 +1,163 @@
-//First Come First Serve CPU Scheduling
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(int argc, char const *argv[])
+struct process_info
 {
-    //Read the total no of processes, and their burst time(taken arrival time of each process=0)
-    int n;
-    printf("Enter the Total no. of process: ");
-    scanf("%d", &n);
+    int max[10];
+    int allocated[10];
+    int need[10];
+};
 
-    //Read the arrival time and burst time of all the processes
-    int at[n], bt[n];
-    printf("Enter the Arrival time of all the processes::\n");
-    for(int i=0; i<n; i++){
-        printf("P[%d]: ", i);
-        scanf("%d", &at[i]);
-    }
+int no_of_process,no_of_resources;
 
-    printf("Enter the Burst time of all the processes::\n");
-    for(int i=0; i<n; i++){
-        printf("P%d]: ", i);
-        scanf("%d", &bt[i]);
+//Take the input 
+void input(struct process_info process[no_of_process],int available[no_of_resources])
+{
+    //Fill array of Structure
+    for(int i=0;i<no_of_process;i++)
+    {
+      printf("Enter process[%d] info\n",i);
+      printf("Enter Maximum Need: ");
+      for(int j=0;j<no_of_resources;j++)
+        scanf("%d",&process[i].max[j]);
+      printf("Enter No. of Allocated Resources for this process: ");
+      for(int j=0;j<no_of_resources;j++)
+      {
+        scanf("%d",&process[i].allocated[j]); 
+        //calculate need/future need 
+        process[i].need[j]= process[i].max[j] - process[i].allocated[j];
+      }
     }
+    printf("Enter Available Resources: ");
+    for(int i=0;i<no_of_resources;i++)
+    {
+      scanf("%d",&available[i]);
+    }
+}
 
-    //execution start time of all the processess
-    int st[n];
-    st[0]=0;
-    for(int i=1; i<n; i++){
-        st[i]=st[i-1]+bt[i-1];
-    }
-    //calculation of waiting time
-    int wt[n];
-    wt[0]=0;
-    for(int i=1; i<n; i++){
-        wt[i]=st[i]-at[i];
-    }
+//Print the Info in Tabular Form
+void showTheInfo(struct process_info process[no_of_process])
+{
+    printf("\nPID\tMaximum\t\tAllocated\tNeed\n");
+    for(int i=0;i<no_of_process;i++)
+    {
+        printf("P[%d]\t",i);
+        for(int j=0;j<no_of_resources;j++)
+           printf("%d ",process[i].max[j]);
+        printf("\t\t");
+        for(int j=0;j<no_of_resources;j++)
+           printf("%d ",process[i].allocated[j]); 
+        printf("\t\t");
+        for(int j=0;j<no_of_resources;j++)
+           printf("%d ",process[i].need[j]);
+        printf("\n");
 
-    //calculation of turn-around time
-    int tat[n];
-    for(int i=0; i<n; i++){
-        tat[i]=bt[i]+wt[i];
     }
+}
 
-    //printing all the values
-    printf("\n-----------------------------------------------------------------------------------------------------------------\n");
-    printf("\tProcess\t\tArrival Time\t\tBurst Time\t\tWait time\t\tTurn-Around Time\n\n");
-    for(int i=0; i<n; i++){
-        printf("\t  P%d\t\t    %d\t\t\t  %d\t\t\t  %d\t\t\t   %d\n\n",i+1, at[i], bt[i], wt[i], tat[i]);
-    }
+//Apply safety algo
+bool applySafetyAlgo(struct process_info process[no_of_process],int available[no_of_resources],int safeSequence[no_of_process])
+{
+   bool finish[no_of_process];
+   int work[no_of_resources];  
+   for(int i=0;i<no_of_resources;i++)
+   {
+      work[i]=available[i];      
+   }
+   for(int i=0;i<no_of_process;i++)
+     finish[i]=false;    
+   bool proceed=true;
+   int k=0;
+   while(proceed)
+   {
+     proceed=false;
+     for(int i=0;i<no_of_process;i++)
+     {    
+         bool flag=true;
+         //Find Index i
+         
+         if(finish[i]==false)
+         {
+           
+           for(int j=0;j<no_of_resources;j++)
+           {
+             //if Need <= Work
+             if(process[i].need[j] <= work[j])
+             {
+               continue;
+             }
+             else
+             {
+               flag=false;  // implies that the current process need > work
+               break;
+             }
+           }
+           if(flag==false)
+             continue;    //check for next process
+          
+          //If we get Index i(or process i), update work  
+          for(int j=0;j<no_of_resources;j++)   
+            work[j]=work[j]+ process[i].allocated[j];
+          finish[i]=true;
+          safeSequence[k++]=i;
+          proceed=true;    // tells that we got atleast one process in safe state, we can proceed
+          
+         }
+       }//end of outer for loop
 
-    float twt=0, ttat=0;
-    for(int i=0; i<n; i++){
-        twt+=wt[i];
-        ttat+=tat[i];
+     } // end of while
+
+     //check finish array
+     int i;
+     for( i=0;i<no_of_process&&finish[i]==true;i++)
+     {
+       continue;
+     }
+     //If all processes are completed, then return true
+     if(i==no_of_process)
+      return true;
+     else
+      return false;
+}
+
+//Checks if we State is safe or not
+bool isSafeState(struct process_info process[no_of_process],int available[no_of_resources],int safeSequence[no_of_process])
+{
+     
+    if(applySafetyAlgo(process,available,safeSequence)==true)
+      return true;
+    return false;
+   
+}
+
+
+int main()
+{
+    printf("Enter No of Process\n");
+    scanf("%d",&no_of_process);
+    printf("Enter No of Resource Instances in system\n");
+    scanf("%d",&no_of_resources);
+    int available[no_of_resources];
+    int safeSequence[no_of_process];
+    //Create Array of Structure to store Processes's Informations
+    struct process_info process[no_of_process];
+   
+    printf("**********Enter details of processes************\n");
+    //Take the Input
+    input(process,available);
+
+    //Print the Info in Tabular Form 
+   // showTheInfo(process);
+    if(isSafeState(process,available,safeSequence))
+    {
+
+      printf("\nSystem is in SAFE State\n");
+      printf("Safe Sequence is: ");
+      for(int i=0;i<no_of_process;i++)
+        printf("P[%d] ",safeSequence[i]);
+  printf("1");
     }
-    printf("\nAverage Waiting time: %f\n", twt/n);
-    printf("\nAverage turn-around time: %f\n\n", ttat/n);
-    return 0;
+    else
+      printf("0");
+    return 0;
 }
