3dd162a5f64b60fd4694aee9531e63d34d478e99
SAMBIT SARGAM EKALABYA <sambitsargam2003@gmail.com>
2022-12-24 04:15:09 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,163 +1,51 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-struct process_info
-{
-    int max[10];
-    int allocated[10];
-    int need[10];
-};
-
-int no_of_process,no_of_resources;
-
-//Take the input 
-void input(struct process_info process[no_of_process],int available[no_of_resources])
-{
-    //Fill array of Structure
-    for(int i=0;i<no_of_process;i++)
-    {
-      printf("Enter process[%d] info\n",i);
-      printf("Enter Maximum Need: ");
-      for(int j=0;j<no_of_resources;j++)
-        scanf("%d",&process[i].max[j]);
-      printf("Enter No. of Allocated Resources for this process: ");
-      for(int j=0;j<no_of_resources;j++)
-      {
-        scanf("%d",&process[i].allocated[j]); 
-        //calculate need/future need 
-        process[i].need[j]= process[i].max[j] - process[i].allocated[j];
-      }
-    }
-    printf("Enter Available Resources: ");
-    for(int i=0;i<no_of_resources;i++)
-    {
-      scanf("%d",&available[i]);
-    }
-}
-
-//Print the Info in Tabular Form
-void showTheInfo(struct process_info process[no_of_process])
-{
-    printf("\nPID\tMaximum\t\tAllocated\tNeed\n");
-    for(int i=0;i<no_of_process;i++)
-    {
-        printf("P[%d]\t",i);
-        for(int j=0;j<no_of_resources;j++)
-           printf("%d ",process[i].max[j]);
-        printf("\t\t");
-        for(int j=0;j<no_of_resources;j++)
-           printf("%d ",process[i].allocated[j]); 
-        printf("\t\t");
-        for(int j=0;j<no_of_resources;j++)
-           printf("%d ",process[i].need[j]);
-        printf("\n");
-
-    }
-}
-
-//Apply safety algo
-bool applySafetyAlgo(struct process_info process[no_of_process],int available[no_of_resources],int safeSequence[no_of_process])
-{
-   bool finish[no_of_process];
-   int work[no_of_resources];  
-   for(int i=0;i<no_of_resources;i++)
-   {
-      work[i]=available[i];      
-   }
-   for(int i=0;i<no_of_process;i++)
-     finish[i]=false;    
-   bool proceed=true;
-   int k=0;
-   while(proceed)
-   {
-     proceed=false;
-     for(int i=0;i<no_of_process;i++)
-     {    
-         bool flag=true;
-         //Find Index i
-         
-         if(finish[i]==false)
-         {
-           
-           for(int j=0;j<no_of_resources;j++)
-           {
-             //if Need <= Work
-             if(process[i].need[j] <= work[j])
-             {
-               continue;
-             }
-             else
-             {
-               flag=false;  // implies that the current process need > work
-               break;
-             }
-           }
-           if(flag==false)
-             continue;    //check for next process
-          
-          //If we get Index i(or process i), update work  
-          for(int j=0;j<no_of_resources;j++)   
-            work[j]=work[j]+ process[i].allocated[j];
-          finish[i]=true;
-          safeSequence[k++]=i;
-          proceed=true;    // tells that we got atleast one process in safe state, we can proceed
-          
-         }
-       }//end of outer for loop
-
-     } // end of while
-
-     //check finish array
-     int i;
-     for( i=0;i<no_of_process&&finish[i]==true;i++)
-     {
-       continue;
-     }
-     //If all processes are completed, then return true
-     if(i==no_of_process)
-      return true;
-     else
-      return false;
-}
-
-//Checks if we State is safe or not
-bool isSafeState(struct process_info process[no_of_process],int available[no_of_resources],int safeSequence[no_of_process])
-{
-     
-    if(applySafetyAlgo(process,available,safeSequence)==true)
-      return true;
-    return false;
-   
-}
-
-
 int main()
 {
-    printf("Enter No of Process\n");
-    scanf("%d",&no_of_process);
-    printf("Enter No of Resource Instances in system\n");
-    scanf("%d",&no_of_resources);
-    int available[no_of_resources];
-    int safeSequence[no_of_process];
-    //Create Array of Structure to store Processes's Informations
-    struct process_info process[no_of_process];
-   
-    printf("**********Enter details of processes************\n");
-    //Take the Input
-    input(process,available);
-
-    //Print the Info in Tabular Form 
-   // showTheInfo(process);
-    if(isSafeState(process,available,safeSequence))
+    char t_table[100][100];
+    char c_sym,c_state;
+    int i,j,n_sym,n_state;
+    printf("Enter the no. symbols in alphabets set \n");
+    scanf("%d",&n_sym);
+    printf("Enter the no. states \n");
+    scanf("%d",&n_state);
+    printf("enter transition table::\n");
+    for(i=0;i<n_state+1;i++)
     {
-
-      printf("\nSystem is in SAFE State\n");
-      printf("Safe Sequence is: ");
-      for(int i=0;i<no_of_process;i++)
-        printf("P[%d] ",safeSequence[i]);
-  printf("1");
+    	for(j=0;j<n_sym+2;j++)
+    	{
+    	scanf("\n");
+    	scanf("%c",&t_table[i][j]);
+    	}
     }
+    for(i=0;i<n_state+1;i++)
+    {
+    	for(j=0;j<n_sym+2;j++)
+    	{    
+    	printf("%c \t",t_table[i][j]); 
+    	}
+    printf("\n");
+    }
+    printf("Enter the string \n");
+    scanf("\n");
+    c_state=t_table[1][0];
+    while((c_sym=getchar())!='\n')
+    {
+    printf("on %c state applying symbol %c ",c_state,c_sym);
+    i=1;
+    	while(c_state!=t_table[i][0])
+    	i++;
+    	j=1;
+    	while(c_sym!=t_table[0][j])
+    	j++;
+    	c_state=t_table[i][j];
+    	printf("goes to %c \n",c_state);
+    }
+    i=1;
+    while((c_state!=t_table[i][0]))
+    i++;
+    if(t_table[i][n_sym+1]=='F')
+    	printf("\n\n Accepted \n\n");
     else
-      printf("0");
-    return 0;
+    	printf("\n\n Not accepted \n\n");
+	return 0;
 }
